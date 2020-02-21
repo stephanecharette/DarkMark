48227437f1bcc55b7cf153c43f2b4a6bb335089c
Stephane Charette <stephanecharette@gmail.com>
2020-02-21 14:17:25 +0000

    use a 'fake' version number when building from tarball

--- CM_version.cmake
+++ CM_version.cmake
@@ -7,6 +7,13 @@ EXECUTE_PROCESS (
 	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 	OUTPUT_VARIABLE DM_VER_SVN
 	OUTPUT_STRIP_TRAILING_WHITESPACE )
+
+IF ( NOT DM_VER_SVN MATCHES "[0-9]+.*" )
+	# Maybe building from tarball without having done a svn checkout?  Use a fake version number
+	SET ( DM_VER_SVN 1 )
+	MESSAGE ( "exact version number is unavailable (building from source tarball?)" )
+ENDIF ()
+
 SET ( DM_VER_MAJOR 1 )
 SET ( DM_VER_MINOR 0 )
 SET ( DM_VER_PATCH 0-${DM_VER_SVN} )
