fe3301d4cb2c0678c86e90882a603ce832a02fc4
Stephane Charette <stephanecharette@gmail.com>
2021-06-06 12:46:29 -0700

    WIP: preliminary work on getting DarkMark to build on Windows (incomplete)

--- CM_definitions.cmake
+++ CM_definitions.cmake
@@ -4,5 +4,14 @@
 SET ( CMAKE_CXX_STANDARD 17 )
 SET ( CMAKE_CXX_STANDARD_REQUIRED ON )
 
-# cannot add the other flags until JUCE has been built
-ADD_DEFINITIONS ( "-Wall" ) # -Wextra -Werror -Wno-unused-parameter" )
+IF (WIN32)
+	ADD_COMPILE_OPTIONS ( /W4 )				# warning level (high)
+	ADD_COMPILE_OPTIONS ( /WX )				# treat warnings as errors
+	ADD_COMPILE_OPTIONS ( /permissive- )	# stick to C++ standards (turn off Microsoft-specific extensions)
+	ADD_COMPILE_OPTIONS ( /wd4100 )			# disable "unreferenced formal parameter"
+	ADD_COMPILE_DEFINITIONS ( _CRT_SECURE_NO_WARNINGS )	# don't complain about localtime()
+	SET ( CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" )
+ELSE ()
+	# cannot add the other flags until JUCE has been built
+	ADD_DEFINITIONS ( "-Wall" ) # -Wextra -Werror -Wno-unused-parameter" )
+ENDIF ()
--- CM_dependencies.cmake
+++ CM_dependencies.cmake
@@ -3,10 +3,22 @@
 
 FIND_PACKAGE ( Threads			REQUIRED	)
 FIND_PACKAGE ( OpenCV	CONFIG	REQUIRED	)
-FIND_LIBRARY ( DARKNET			darknet		)
-FIND_LIBRARY ( DARKHELP			darkhelp	)
-FIND_LIBRARY ( LIBMAGIC			magic		) # sudo apt-get install libmagic-dev
-FIND_PACKAGE ( GTest			QUIET		)
+
+IF (WIN32)
+	# Assume that vcpkg was used on Windows
+	FIND_PACKAGE (Darknet REQUIRED)
+	INCLUDE_DIRECTORIES (${Darknet_INCLUDE_DIR})
+	SET (Darknet Darknet::dark)
+	FIND_LIBRARY ( DARKHELP darkhelp REQUIRED HINTS  ../DarkHelp/build/src-lib/Release )
+	INCLUDE_DIRECTORIES ( ../DarkHelp/src-lib )
+	SET (LIBMAGIC "")
+ELSE ()
+	FIND_LIBRARY ( DARKNET			darknet		)
+	FIND_LIBRARY ( DARKHELP			darkhelp	)
+	FIND_LIBRARY ( LIBMAGIC			magic		) # sudo apt-get install libmagic-dev
+	FIND_PACKAGE ( GTest			QUIET		)
+ENDIF ()
+
 
 SET ( DM_LIBRARIES Threads::Threads ${DARKHELP} ${DARKNET} ${OpenCV_LIBS} ${LIBMAGIC} )
 
