a2fdd8cd6e0a3fea1eb2fe62eff68a6f02ee1962
Stéphane Charette <stephanecharette@gmail.com>
2023-02-18 23:12:34 -0800

    fix broken Ubuntu package (regression from recent WIN32 changes)

--- CM_package_ubuntu.cmake
+++
@@ -1,9 +0,0 @@
-# DarkMark (C) 2019-2023 Stephane Charette <stephanecharette@gmail.com>
-
-
-SET ( CPACK_DEBIAN_PACKAGE_SECTION			"other" )
-SET ( CPACK_DEBIAN_PACKAGE_PRIORITY			"optional" )
-SET ( CPACK_DEBIAN_PACKAGE_DEPENDS			"darkhelp" )
-SET ( CPACK_DEBIAN_PACKAGE_MAINTAINER		"Stephane Charette <stephanecharette@gmail.com>" )
-SET ( CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA 	"${CMAKE_CURRENT_SOURCE_DIR}/src-ubuntu/postinst;${CMAKE_CURRENT_SOURCE_DIR}/src-ubuntu/postrm" )
-SET ( CPACK_GENERATOR						"DEB" )
